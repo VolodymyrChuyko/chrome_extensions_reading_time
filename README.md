daf5dccfe416e7b5150fe6b9bfcb120a89034839
Volodymyr Chuiko <volodymyr.chuiko@clean.io>
2023-03-04 14:52:01 +0200

    Add extension

---
+++ README.md
@@ -0,0 +1,20 @@
+#Reading time
+
+This extension adds the expected reading time to any Chrome extension and Chrome Web Store documentation page.
+
+The extension will first check if the page contains the ```<article>``` element. Then, it will count all the words within this element and create a paragraph that displays the total reading time.
+
+   - Load your extension locally
+   - Open an extension or Chrome Web Store documentation
+
+   Here are a few pages you can open to see how long each article will take to read.
+
+   [Welcome to the Chrome Extension documentation](https://developer.chrome.com/docs/extensions/mv3/)
+
+   [Publish in the Chrome Web Store](https://developer.chrome.com/docs/webstore/publish/)
+
+   [Understanding Content Scripts](https://developer.chrome.com/docs/extensions/mv3/content_scripts/)
+
+   [Chrome DevTools](https://developer.chrome.com/docs/devtools/)
+
+   [Workbox](https://developer.chrome.com/docs/workbox/)
---
+++ scripts/content.js
@@ -0,0 +1,41 @@
+const article = document.querySelector("article");
+const ul = document.querySelector("ul");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+} else
+
+
+// this pattern was added to support other chrome developer pages, like for example, the Chrome DevTools or Workbox
+if (ul) {
+  const text = ul.textContent;
+  const wordMatchRegExp = /[^\s]+/g;
+  const words = text.matchAll(wordMatchRegExp);
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  const heading = document.querySelector("h1");
+  const date = document.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
